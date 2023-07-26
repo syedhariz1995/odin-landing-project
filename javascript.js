20696964406114df15e39e6becdd342130ed63aa
Hariz <syedhariz1995@gmail.com>
2023-07-26 10:48:34 +0800

    added JavaScript file for JavaScript personal practice

---
+++ javascript.js
@@ -0,0 +1,115 @@
+// .toString()     // Convert number to string
+
+// let num = 123;
+// let convertString = num.toString()
+// console.log(convertString)           // "123"
+// console.log(typeof convertString)    // String
+
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+
+//Number()      // Covert String to Number
+
+// let str = "123"
+// let converNum = Number(str)
+// console.log(converNum)           // 123
+// console.log(typeof converNum)    // Number
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+// .length     // Find out the length of characters
+
+// let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let length = text.length;
+// console.log(length) // 26
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+// .slice(start,end)    // Extracts parts of string
+
+
+//EX1
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(7, 13); // .slice(include, exclude)
+// console.log(part) //Banana
+
+
+//EX2
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(7); // .slice(start till end)
+// console.log(part) //Banana, Kiwi
+
+
+
+//EX3
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(-12); // .slice(backwards / from end of string)
+// console.log(part)
+
+
+//EX4
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(-12, -6);
+// console.log(part)
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+// .substr (start, length)      // Same as slice, but second parameter is the length
+
+// let str = "Apple, Banana, Kiwi";
+// let part = str.substr(7, 4);  //Start from index 7, but only give length of 4 after it.
+// console.log(part) // Bana
+
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
