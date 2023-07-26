9e0880f0ff7acf10d09d8ae02d838da9bfa7f83c
Hariz <syedhariz1995@gmail.com>
2023-07-26 17:00:50 +0800

    added more notes in JS

--- javascript.js
+++ javascript.js
@@ -19,7 +19,7 @@
 
 
 
-//Number()      // Covert String to Number
+//Number(variable inside)      // Covert String to Number
 
 // let str = "123"
 // let converNum = Number(str)
@@ -111,5 +111,196 @@
  * 
  * 
  */
+
+// .replace("first match", "changed")    // Replace string  // Also case sensitive   
+// use /word/g for global, and /word/i for insensitive case
+
+// let text = "Please visit Microsoft!";
+// let newText = text.replace("Microsoft", "W3Schools");
+// console.log(newText) // "Please visit W3Schools"
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+// .toUpperCase() and .toLowerCase()   // Convert lower case string to upper case and vice versa
+
+
+// Uppercase
+// let text1 = "Hello World!";
+// let text2 = text1.toUpperCase();
+// console.log(text2) // "HELLO WORLD!"
+
+
+// Lowercase
+// let text1 = "Hello World!";       
+// let text2 = text1.toLowerCase();
+// console.log(text2) //"hello world!"
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+// .trim()   // Removes whitespace from both side of string
+
+// let text1 = "      Hello World!      ";
+// let text2 = text1.trim();
+// console.log(text2)
+
+
+//Note : trimStart() and trimEnd() exist. Both removes white space from start of string or end of string respectively.
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+// .charAt(string index)    // To show specific character based on index
+
+// let text = "HELLO WORLD";
+// let char = text.charAt(6);
+// console.log(char) // W
+
+
+
+ // or use property access with []
+
+
+// let text = "HELLO WORLD";
+// let char = text[6];
+// console.log(char)
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+// .split("")    // Split for each character
+// .split(" ")   // Split on space (each word)          // Convert string into arrays
+// .split(",")   // Split on commas  (One index)
+
+
+// let text = "HELLO WORLD";
+// let char = text.split(" ");
+// console.log(char) // ["HELLO", "WORLD"]
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+// .indexOf()     // Returns position of substring in main string, -1 if not found
+
+// let text = "Please locate where 'locate' occurs!";
+// let index = text.indexOf("locate");
+// console.log(index) // 7
+
+
+
+// .lastIndexOf()   // Last occurence instead.
+
+// let text = "Please locate where 'locate' occurs!";
+// let index = text.lastIndexOf("locate");
+// console.log(index); // 21
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+// .search()     // More versatile to search index, but no 2nd parameter
+
+// let text = "Please locate where 'locate' occurs!";
+// let textSearch = text.search("locate");
+// let textIndex = text.indexOf("locate")
+// console.log(textSearch) // 7
+// console.log(textIndex) // 7
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+// .match("word")    // look for a match in string, returns the index
+
+// let text = "The rain in SPAIN stays mainly in the plain";
+// let match = text.match("ain");
+// console.log(match)
+
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+// .includes("word", starting index)   // Returns true (or false) if string contains specific value.
+// also case sensitive
+// also check startsWith() and endsWith(). return true for first and last word respectively
+
+// let text = "Hello world, welcome to the universe.";
+// let include = text.includes("world");
+// console.log(include) // true
+
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
 
 
