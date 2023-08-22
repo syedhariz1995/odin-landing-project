d4faf2d90d3625f627f8ee4bbcbef07e5a2bd49c
MiraNadhirah <56228371+MiraNadhirah@users.noreply.github.com>
2023-08-22 16:19:57 +0800

    test again

--- javascript.js
+++ javascript.js
@@ -1243,21 +1243,23 @@
 
 
 
-const convertToCelsius = function(temp) {
-    return Math.round((temp - 32) * (5/9) * 10) / 10
-  };
+// const convertToCelsius = function(temp) {
+//     return Math.round((temp - 32) * (5/9) * 10) / 10
+//   };
   
-  const convertToFahrenheit = function(temp) {
-    return Math.round((temp* (9/5) + 32) * 10) / 10
-  };
-  
-  
-  console.log(convertToCelsius(32))
-  console.log(convertToCelsius(100)) 
-  console.log(convertToCelsius(-100)) 
+//   const convertToFahrenheit = function(temp) {
+//     return Math.round((temp* (9/5) + 32) * 10) / 10
+//   };
   
   
-  console.log(convertToFahrenheit(0)) 
-  console.log(convertToFahrenheit(73.2)) 
-  console.log(convertToFahrenheit(-10)) 
+//   console.log(convertToCelsius(32))
+//   console.log(convertToCelsius(100)) 
+//   console.log(convertToCelsius(-100)) 
+  
+  
+//   console.log(convertToFahrenheit(0)) 
+//   console.log(convertToFahrenheit(73.2)) 
+//   console.log(convertToFahrenheit(-10)) 
+
+
 
