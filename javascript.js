cc6d48a49a848018f523d4f431f31a7d67e4677d
Hariz <syedhariz1995@gmail.com>
2023-08-22 23:14:58 +0800

    added some notes for DOM

--- javascript.js
+++ javascript.js
@@ -1163,6 +1163,14 @@
 
 
 
+// /**
+//  * 
+//  * 
+//  * 
+//  * 
+//  * 
+//  * 
+//  */
 
 
 
@@ -1263,4 +1271,59 @@
 
 
 
-// just a bit more
+
+
+
+
+
+// /**
+//  * 
+//  * 
+//  * 
+//  * 
+//  * 
+//  * 
+//  */
+
+
+
+
+
+// DOM - Document Object Model
+// NOTE : this is the html for practice in index.html near the bottom
+
+{/* <div id="container">
+    <div class="display"></div>
+    <div class="controls"></div>
+</div> */}
+
+// use to manipulate the page using JavaScript
+
+// Target nodes with selector 
+
+// example : 
+
+// const container = document.querySelector('#container') // selects the #container div 
+
+// console.dir(container.firstElementChild);  // selects the first child of #container => .display                    
+
+// const controls = document.querySelector('.controls');  // selects the .controls div
+
+// console.dir(controls.previousElementSibling); // selects the prior sibling => .display
+
+
+
+// Another note : 
+
+// element.querySelector(selector)   // returns a reference to the first match of selector
+
+// element.querySelectorAll(selectors) //  returns a “nodelist” containing references to all of the matches of the selectors
+
+
+
+
+
+
+// Create element
+
+// const div = document.createElement('div'); 
