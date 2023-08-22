5271816b76ab19b78fb019892e2c42067a270ce6
MiraNadhirah <56228371+MiraNadhirah@users.noreply.github.com>
2023-08-22 16:15:27 +0800

    added exercise from TOP

--- javascript.js
+++ javascript.js
@@ -1232,3 +1232,32 @@
 
 // year % 100 !== 0  are not leap year unless its year % 400 === 0
  
+
+
+
+
+
+// Exercise 4
+
+
+
+
+
+const convertToCelsius = function(temp) {
+    return Math.round((temp - 32) * (5/9) * 10) / 10
+  };
+  
+  const convertToFahrenheit = function(temp) {
+    return Math.round((temp* (9/5) + 32) * 10) / 10
+  };
+  
+  
+  console.log(convertToCelsius(32))
+  console.log(convertToCelsius(100)) 
+  console.log(convertToCelsius(-100)) 
+  
+  
+  console.log(convertToFahrenheit(0)) 
+  console.log(convertToFahrenheit(73.2)) 
+  console.log(convertToFahrenheit(-10)) 
+
