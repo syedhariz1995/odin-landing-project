7d140c3f66c9d29a520ab4fdf5f51a213ab39272
syedhariz1995 <syedhariz1995@gmail.com>
2023-10-09 14:20:12 +0800

    added array objects practice

--- javascript.js
+++ javascript.js
@@ -3327,3 +3327,22 @@
 
 
 
+// Array methods with objects
+// Practice : use these object arrays
+
+const inventors = [
+  {first: 'Albert', last: 'Einstein', year: 1879, passed: 1955},
+  {first: 'Isaac', last: 'Newton', year: 1643, passed: 1727},
+  {first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642},
+  {first: 'Marie', last: 'Curie', year: 1867, passed: 1934},
+  {first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630},
+  {first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543},
+  {first: 'Max', last: 'Planck', year: 1858, passed: 1947},
+]
+
+const people = ['Beck, Glen', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Wick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 
+'Bellow, Saul', 'Benchley, Robert', 'Beneson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 
+'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irvin', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 
+'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine',
+'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William']
+
